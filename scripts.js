22a67ebe5f5f7c0760967d1646a2d653b9804f26
chenste-osu <chen.st.we@gmail.com>
2023-07-26 19:08:12 -0400

    add form to update table

--- scripts.js
+++ scripts.js
@@ -11,26 +11,20 @@ class Library {
     }
 }
 
-let myLibrary = new Library();
-
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author; 
     this.pages = pages;
-    this.read = read;
+    this.read = "No";
+
+    if (read == true) {
+        this.read = "Yes"
+    } 
 
     this.info = function() {
         return [this.title, this.author, this.pages, this.read];
     }
 }
-
-let papan = new Book("papan", "natsuko", 250, true);
-let sixteen = new Book("ff16", "yoship", 420, false);
-let snowdrops = new Book("A. D. Miller", "Snowdrops", 272, false);
-
-myLibrary.addBookToLibrary(papan);
-myLibrary.addBookToLibrary(sixteen);
-myLibrary.addBookToLibrary(snowdrops);
 
 function getBodyHTML() {
     let libraryTable = document.getElementById("libraryTable")
@@ -52,5 +46,52 @@ function addBookDataHTML(book) {
         newCell.appendChild(newText);
     }
 }
+
+function openForm() {
+    document.getElementById("formContainer").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("formContainer").style.display = "none";
+}
+
+let myLibrary = new Library();
+
+let papan = new Book("papan", "natsuko", 250, true);
+let sixteen = new Book("ff16", "yoship", 420, false);
+let snowdrops = new Book("A. D. Miller", "Snowdrops", 272, false);
+
+myLibrary.addBookToLibrary(papan);
+myLibrary.addBookToLibrary(sixteen);
+myLibrary.addBookToLibrary(snowdrops);
+
+
+let bookForm = document.getElementById("bookForm");
+bookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let bookForm = document.getElementById("bookForm");
+    let formResponses = new FormData(bookForm);
+
+    let newTitle = formResponses.get("title");
+    let newAuthor = formResponses.get("author");
+    let newPages = formResponses.get("pages");
+    let newRead = formResponses.get("readStatus");
+
+    let addBook = new Book(newTitle, newAuthor, newPages, newRead);
+    myLibrary.addBookToLibrary(addBook);
+
+    // let title = document.getElementById("formTitle");
+    // let author = document.getElementById("formAuthor");
+    // let pages = document.getElementById("formPages");
+    // let readRadio = document.getElementsByName("readStatus");
+    // let readStatus = false; 
+
+    // if (readRadio[0] == "true") {
+    //     readStatus = true;
+    // } 
+
+    // console.log([title, author, pages, readStatus].join(""));
+})
 
 
